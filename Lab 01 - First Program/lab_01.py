fd5b94e9ca94da7d79ae9cafe7972fa07cf5cb35
Max Diaz <max.diaz@my.simpson.edu>
2020-09-02 22:49:10 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,24 @@
-print("Hello World")
+print("Welcome to this Brave New World!")
+
+print("The car has different license plates.")
+
+print("Mike said,\"The license plate is from Wisconsin.\"")
+
+print("Julius Caesar said,\"Veni vidi vici\", after a speedy victory in the Battle of Zela against the King of Pontus.")
+
+print("John\nlikes\nto\neat\ntamales\nand\na\ncoke.")
+
+print("\tI\tLike\treading\tbooks.")
+
+print("My favorite drink is \rcoke")
+
+print("Johnathan stated,\"My favorite is \'1984\' due to my interest in dystopias.\"")
+
+print("""Diego's 
+Handwriting 
+isn't 
+the 
+best but it's 
+better 
+than 
+Jose's.""")
